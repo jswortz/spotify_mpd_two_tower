4926c14ff207c6252445120d39e983bfbf4a0822
tottenjordan <jv.tott@gmail.com>
2022-12-29 21:51:55 +0000

    nvtop installed train

--- src/two_tower_jt/two_tower.py
+++ src/two_tower_jt/two_tower.py
@@ -19,7 +19,8 @@ from pprint import pprint
 # DROPOUT_RATE=0.33
 # MAX_TOKENS=20000 #50000
 
-from . import train_config as cfg
+# from . import train_config as cfg
+import train_config as cfg
 
 EMBEDDING_DIM = cfg.EMBEDDING_DIM       # 128
 PROJECTION_DIM = cfg.PROJECTION_DIM     # 50
